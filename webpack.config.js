b6a9ab4660a984f36de63e15107c6af085eb4fec
Priya <priyyavarun@gmail.com>
2022-08-31 09:17:22 +0530

    MF changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const dependencies = require("./package.json").dependencies;
 
 module.exports = {
   entry: "./src/index.js",
@@ -48,12 +49,20 @@ module.exports = {
       shared: {
         "react": {
           singleton: true,
-          requiredVersion: "18.2.0"
+          requiredVersion: dependencies.react
         },
         "react-dom": {
           singleton: true,
-          requiredVersion: "18.2.0"
-        }
+          requiredVersion: dependencies["react-dom"]
+        },
+        "@mui/material": {
+          singleton: true,
+          requiredVersion: dependencies["@mui/material"]
+        },
+        "@mui/icons-material": {
+          singleton: true,
+          requiredVersion: dependencies["@mui/icons-material"]
+        },
       }
     })
   ],
